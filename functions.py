c2966f4773c889ae59ba890fb9b6cb901961e7e5
GilmanovAS <albertgilmanov8@gmail.com>
2023-09-11 17:21:02 +0500

    functions

--- functions.py
+++ functions.py
@@ -1,1 +1,6 @@
 import json
+
+def load_json():
+    with open('posts.json', 'r', encoding='utf8') as fp:
+
+    return
--- main_bp/main_view.py
+++ main_bp/main_view.py
@@ -1,7 +1,11 @@
 from flask import render_template, Blueprint
 
-main_blueprint = Blueprint('main_blueprint', __name__)
+main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
 
 @main_blueprint.route('/')
 def main_page():
     return render_template('index.html')
+
+@main_blueprint.route('/search/')
+def search_page():
+    return render_template('post_list.html')
